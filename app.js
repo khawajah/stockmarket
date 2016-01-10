af23cd3c7f284ca1086141560d1bf337a340d6de
Walter Cavalcante Junior <waltercavalcantejunior@gmail.com>
2016-01-10 18:48:40 -0200

    remove favicon.ico
    Removing because heroku it's not finding the icon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -13,7 +13,7 @@ var io = require('socket.io')(http);
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
